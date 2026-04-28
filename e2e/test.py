39167a1b6b5ec3a05b3b0945fb9de58e02ec767e
Vanessa <vanessa.glezalonso92@gmail.com>
2026-04-28 15:38:03 +0200

    test

--- e2e/test.py
+++ e2e/test.py
@@ -6,7 +6,7 @@ def test_login(page):
     page.fill('#user-name', 'standard_user')
     page.fill("#password", "secret_sauce")
     page.click("#login-button")
-    inventory_container = page.locator("#inventory_container")
+    inventory_container = page.locator("#inventory_container").first
     assert inventory_container.is_visible()
 
 
